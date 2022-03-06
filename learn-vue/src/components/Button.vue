787cc1abc6f5571ba09c9a73da10fa58492e2cb0
Kenneth Adjei <[kmadjei25@gmail.com]>
2022-03-06 17:04:26 -0500

    started task manager project

---
+++ learn-vue/src/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+    <button 
+        class="btn"
+        :style="{ background: color}"
+        @click="onClick()"
+    >
+        Add Task
+    </button>
+</template>
+
+<script>
+    export default {
+        name: 'Button',
+        props: {
+            text: String,
+            color: String
+        },
+    }
+</script>
